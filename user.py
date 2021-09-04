7202cf02bc78caa8692f4925ffe563b9251e4ae8
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-09-04 11:40:26 +0300

    fid user by username from users array done

--- user.py
+++ user.py
@@ -23,7 +23,12 @@ class User():
         """
         User.user_accounts.remove(self)
 
-
+    @classmethod
+    def find_user(cls, username):
+        """Find user by username"""
+        for user in cls.user_accounts:
+            if user.username == username:
+                return user
 
     @classmethod
     def user_exist(cls, username):
